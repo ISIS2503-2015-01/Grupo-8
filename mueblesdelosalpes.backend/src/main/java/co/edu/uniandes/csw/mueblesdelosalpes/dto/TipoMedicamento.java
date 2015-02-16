57ae589ff24dcbf8b60b36afc8025f589a614976
cristian25cd <cd.copete461@uniandes.edu.co>
2015-02-16 13:27:42 -0500

    First commit

---
+++ mueblesdelosalpes.backend/src/main/java/co/edu/uniandes/csw/mueblesdelosalpes/dto/TipoMedicamento.java
@@ -0,0 +1,27 @@
+/**
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * $Id$ TipoMueble.java
+ * Universidad de los Andes (Bogotá - Colombia)
+ * Departamento de Ingeniería de Sistemas y Computación
+ * Licenciado bajo el esquema Academic Free License version 3.0
+ *
+ * Ejercicio: Muebles de los Alpes
+ * Autor: Juan Sebastián Urrego
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+
+package co.edu.uniandes.csw.mueblesdelosalpes.dto;
+
+/**
+ * Clase de presentaciones que ofrece el medicamento
+ * @author David Mayorga
+ */
+public enum TipoMedicamento
+{
+    Tableta,
+    Suspension, //jarabe
+    Inyectable,
+    Topico, //cremas
+    Supositorio
+    
+}
