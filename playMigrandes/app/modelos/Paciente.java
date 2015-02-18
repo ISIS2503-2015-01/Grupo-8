b14b5ec0fe5c11e69b0207ed02c0a94e9c66c3c9
cristian25cd <cd.copete461@uniandes.edu.co>
2015-02-18 13:44:24 -0500

    play app

---
+++ playMigrandes/app/modelos/Paciente.java
@@ -0,0 +1,186 @@
+package modelos;
+
+/**
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * $Id$ Paciente.java
+ * Universidad de los Andes (Bogotá - Colombia)
+ * Departamento de Ingeniería de Sistemas y Computación
+ * Licenciado bajo el esquema Academic Free License version 3.0
+ *
+ * Ejercicio: Muebles de los Alpes
+ * Autor: Juan Sebastián Urrego
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Clase que modela un paciente dentro del sistema.
+ * @author Juan Sebastián Urrego
+ */
+public class Paciente
+{
+
+    //-----------------------------------------------------------
+    // Atributos
+    //-----------------------------------------------------------
+
+    /**
+     * Número de identificación del paciente
+     */
+    private long id;
+
+    /**
+     * Nombres del paciente.
+     */
+    private String nombres;
+
+ 
+
+    /**
+     * Lista de ítems de actividades del paciente.
+     */
+    private List<Actividad> actividades;
+    
+    
+    /**
+     * Lista de ítems de episodios del paciente.
+     */
+    private List<Episodio> episodios;
+
+    /**
+     * Usuario del paciente.
+     */
+    private String usuario;
+    
+    /**
+     * Telefono del paciente
+     */
+    private String telefono;
+
+    /**
+     * Perfil de paciente.
+     */
+    private String perfil;
+
+    /**
+     * Foto del paciente.
+     */
+    private String foto;
+
+    //-----------------------------------------------------------
+    // Constructor
+    //-----------------------------------------------------------
+
+    /**
+     * Constructor sin argumentos
+     */
+    public Paciente()
+    {
+        actividades = new ArrayList<Actividad>();
+    }
+
+    /**
+     * Constructor con argumentos de la clase. 
+     * @param id Identificador único del paciente
+     * @param nombres Nombre(s) del paciente
+     * @param usuario Username del paciente
+     * @param salario Salario del paciente
+     * @param comisionVentas Valor en comisión por ventas
+     * @param perfil Perfil del paciente
+     * @param foto Nombre de la foto del paciente
+     * @param telefono telefono del paciente
+     */
+    public Paciente(long id, String nombres, String usuario, double salario, double comisionVentas, String perfil, String foto, String telefono)
+    {
+        this.id = id;
+        this.nombres = nombres;
+        this.usuario = usuario;
+        this.actividades = new ArrayList<Actividad>();
+        this.episodios = new ArrayList<Episodio>();
+        this.telefono = telefono;
+        this.perfil = perfil;
+        this.foto = foto;
+    }
+
+    //-----------------------------------------------------------
+    // Getters y setters
+    //-----------------------------------------------------------
+
+    /**
+     * Devuelve el número único de identificación del paciente
+     * @return id Número de identificación
+     */
+    public long getIdentificacion()
+    {
+        return id;
+    }
+
+    /**
+     * Modifica el número de identificación del cliente
+     * @param id Nuevo número de identificación
+     */
+    public void setIdentificacion(long id)
+    {
+        this.id = id;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getPerfil() {
+        return perfil;
+    }
+
+    public void setPerfil(String perfil) {
+        this.perfil = perfil;
+    }
+
+   
+    public void setItemActividad(Actividad actividad)
+    {
+        this.actividades.add(actividad);
+    }
+
+    public List<Actividad> getActividades() {
+        return actividades;
+    }
+
+    public void setActividades(List<Actividad> actividades) {
+        this.actividades = actividades;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+ 
+    
+    
+}
---
+++ playMigrandes/target/scala-2.11/src_managed/main/controllers/routes.java
@@ -0,0 +1,26 @@
+// @SOURCE:C:/Programas/activator/playMigrandes/conf/routes
+// @HASH:d42bfdab9ee9ad83dd64407373b46e2c77b32444
+// @DATE:Wed Feb 18 11:30:09 COT 2015
+
+package controllers;
+
+public class routes {
+public static final controllers.ReversePacienteController PacienteController = new controllers.ReversePacienteController();
+public static final controllers.ReverseAssets Assets = new controllers.ReverseAssets();
+public static final controllers.ReverseApplication Application = new controllers.ReverseApplication();
+
+public static class javascript {
+public static final controllers.javascript.ReversePacienteController PacienteController = new controllers.javascript.ReversePacienteController();
+public static final controllers.javascript.ReverseAssets Assets = new controllers.javascript.ReverseAssets();
+public static final controllers.javascript.ReverseApplication Application = new controllers.javascript.ReverseApplication();
+}
+          
+
+public static class ref {
+public static final controllers.ref.ReversePacienteController PacienteController = new controllers.ref.ReversePacienteController();
+public static final controllers.ref.ReverseAssets Assets = new controllers.ref.ReverseAssets();
+public static final controllers.ref.ReverseApplication Application = new controllers.ref.ReverseApplication();
+}
+          
+}
+          
