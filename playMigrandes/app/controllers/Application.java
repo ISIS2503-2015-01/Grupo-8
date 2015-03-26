9dab3ce7e8602cb83cc2962f177a78b07c6393c2
Diego Farfán <diego_farfan20@hotmail.com>
2015-03-26 00:36:40 -0500

    Commit de controladores e interfaz

--- playMigrandes/app/controllers/Application.java
+++ playMigrandes/app/controllers/Application.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.persistence.Query;
 
+import modelos.Medicamento;
 import modelos.Paciente;
 
 import com.fasterxml.jackson.databind.JsonNode;
@@ -19,38 +20,41 @@ public class Application extends Controller {
 
     public static Result index() 
     {
-        return ok("ola ke ase");
+    	  return ok(index.render("ola ke ase"));
+    	  
     }
     
     @Transactional
 	@BodyParser.Of(BodyParser.Json.class)
     public static Result login()
     {
-    	Paciente n=null;
-    	JsonNode nodo = Controller.request().body().asJson();
-    	String user=nodo.findPath("usuario").asText(); 
-    	String password=nodo.findPath("password").asText();
-    	
-    	
-    	Query q=JPA.em().createQuery("from Paciente p where p.usuario=:usr");
-    	q.setParameter("usr", user);
-    	
-    	List<Paciente> l=q.getResultList();
-    	
-		if(l.size()==0)
-			return badRequest("El usuario no se encuentra registrado");
+//    	Paciente n=null;
+//    	JsonNode nodo = Controller.request().body().asJson();
+//    	String user=nodo.findPath("usuario").asText(); 
+//    	String password=nodo.findPath("password").asText();
+//    	
+//    	
+//    	Query q=JPA.em().createQuery("from Paciente p where p.usuario=:usr");
+//    	q.setParameter("usr", user);
+//    	
+//    	List<Paciente> l=q.getResultList();
+//    	
+//		if(l.size()==0)
+//			return badRequest("El usuario no se encuentra registrado");
+//		
+//		else
+//		{
+//			n=l.get(0);
+//			if(n.getPassword().equals(password))
+//				session(n.getUsuario());
+//	
+//			else
+//				return badRequest("Password incorrecto");
+//		}
 		
-		else
-		{
-			n=l.get(0);
-			if(n.getPassword().equals(password))
-				session(n.getUsuario());
-	
-			else
-				return badRequest("Password incorrecto");
-		}
-		
-		 return Results.created(Json.toJson(n));
-    	
+//		 return Results.created(Json.toJson(n));
+    
+    	return redirect(controllers.routes.PacienteController.getAll());
     }
+    
 }
